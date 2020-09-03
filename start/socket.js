4f1a66e9e5010b3edc2ca9c5f4d0f7637d142ec5
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2020-09-03 03:41:49 +0000

    chore: get redis configuration from env variable

--- start/socket.js
+++ start/socket.js
@@ -1,8 +1,12 @@
 const Server = use('Server');
+const Env = use('Env');
+
 module.exports = io = use('socket.io')(Server.getInstance());
+
 const redisAdapter = require('socket.io-redis');
 const AuthManager = require('../app/AuthManager');
-io.adapter(redisAdapter({host: 'localhost', port: 6379}));
+
+io.adapter(redisAdapter({host: Env.get('REDIS_HOST'), port: ENV.get('REDIS_PORT')}));
 
 io.use(async (socket, next) => {
     const error = new Error('Authentication failed');

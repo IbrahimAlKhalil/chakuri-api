67b4a7adc16b84058d29362a0f27a5ef5b1a5909
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2020-09-04 05:09:48 +0000

    Update start/redis.js

--- start/redis.js
+++ start/redis.js
@@ -1,12 +1,11 @@
 const redis = require('redis');
 const bluebird = require('bluebird');
-const Env = use('Env');
 
 bluebird.promisifyAll(redis);
 
 module.exports = function () {
     return new Promise(resolve => {
-        const client = redis.createClient({host: Env.get('REDIS_HOST'), port: ENV.get('REDIS_PORT')});
+        const client = redis.createClient({host: process.env.REDIS_HOST, port: process.env.REDIS_PORT});
 
         client.select('khidmat', () => {
             resolve(client);
